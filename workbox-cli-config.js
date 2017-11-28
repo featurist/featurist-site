793e3b9c21467ab1c115c49d07372ed5ad91720f
Alec Rust <me@alecrust.com>
2017-11-28 19:01:20 +0000

    Make it a PWA
    Uses workbox-cli to generate the service worker file (and assets list inside) as part of build script.
    
    - Rename `icon-sprite.svg` to `_icon-sprite.svg` to prevent inclusion in `/dist`
    - Rename only `jpeg` instance to `jpg`
    - Add necessary meta tags/app icon images to cover all devices/browsers (keep Lighthouse happy)

---
+++ workbox-cli-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: ['**/*.{html,jpg,png,svg,js,css}'],
+  swDest: 'dist/service-worker.js',
+  globIgnores: ['../workbox-cli-config.js']
+}
