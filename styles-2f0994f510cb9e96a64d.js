59760a17667f7b5c9c412a0ee72f870105734094
artemave <artemave@gmail.com>
2022-01-12 11:29:14 +0100

    Updates

--- styles-2f0994f510cb9e96a64d.js
+++ styles-2f0994f510cb9e96a64d.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{195:function(n,o,w){},231:function(n,o,w){},258:function(n,o,w){}}]);
-//# sourceMappingURL=styles-6357b6cf5836bd2a54fd.js.map
+//# sourceMappingURL=styles-2f0994f510cb9e96a64d.js.map
